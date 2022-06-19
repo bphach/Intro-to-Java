2c2ae12cd92c75ed59b4ebc65d9572b433566f6d
bphach <soturota@gmail.com>
2022-06-19 11:04:32 -0400

    commit

---
+++ src/Lesson04_Math/Math.java
@@ -0,0 +1,23 @@
+package Lesson04_Math;
+
+import java.util.Random;
+
+public class Math {
+
+    public static void main(String[] args){
+
+       
+      Random random = new Random();
+
+      int x = random.nextInt(6)+1;//I'm limiting this to the numbers on a dice. Since computers starts with 0, if you want 6 you must add 1
+      double y = random.nextDouble();
+      boolean z = random.nextBoolean();
+
+      System.out.println(x);
+
+
+
+    }
+
+    }
+   
---
+++ src/Lesson04_Math/main.java
@@ -0,0 +1,28 @@
+package Lesson04_Math;
+
+public class main {
+    public static void main(String[] args) {
+    //Highlighted "Error message" above doens't mean anything.
+    /**
+     *  Key Shortcuts you need to use.
+     *  type these out btw.
+     * 
+     *  sysout = System.out.println();
+     *  main = public static void main(String[] args) {}
+     *  use tab or shift-tab to indent or space out ur lines
+     *  ctrl z to undo, ctrl-shift-z to redo.
+     *  ctrl s to save
+     * 
+     *  // use // to make comments OR /** 
+     */
+        ;
+
+                    
+        
+
+
+    }
+
+
+
+}
