cbef6cd47fa7b9b4752ea8c9486217df7ffd0b62
bphach <soturota@gmail.com>
2022-06-23 12:20:04 -0400

    commit

--- src/Lesson07_Logical_Operators/main.java
+++ src/Lesson07_Logical_Operators/main.java
@@ -1,8 +1,45 @@
 package Lesson07_Logical_Operators;
 
+import java.util.Scanner;
+
 public class main {
     public static void main(String[] args) {
         
+    // logical operators = used to connect two or more expressions 
+    //  && = (AND) both conditions must be true 
+    //  || = (OR) either condtions must be true 
+    //  ! = (NOT) reverses boolean value of condition
+
+        //int temp = 12;
+
+        //if(temp>90) {
+
+            //System.out.println("It is hot outside");
+      //  }
+      //  else if(temp>=80 && temp<=90) {
+            //System.out.println("It is warm outside");
+      //  }
+       // else {
+            //System.out.println("It is freezing!");
+       // }
+
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("You are playing a game! Press q or Q to quit");
+
+        String response = scanner.next(); 
+
+        if(response.equals("q") || !response.equals(null) {
+            System.out.println("You quit the game");
+        }
+        else {
+            System.out.println("You are still playing *pew pew* rascal!");
+        }
+
+
+
+
 
 
     }
