142976aa106bc10614396fcd85dc9e156f0fd8bf
bphach <soturota@gmail.com>
2022-06-25 09:50:58 -0400

    commit

--- src/Lesson09_For_Loop/main.java
+++ src/Lesson09_For_Loop/main.java
@@ -2,8 +2,16 @@ package Lesson09_For_Loop;
 
 public class main {
     public static void main(String[] args) {
-       
+       //for loop = executes a block of code a limited amount of times
+       //To create a for loop, you type in for and add a condtion with two other statements in the parentheses 
+       //index = i
+       //this for loop has 3 statements. First one declare an counter or index call i. Our condition is i is less then or equal to 0, final statement is to decrement our index 
 
+       for(int i=10; i>=0; i++) {
+            System.out.println(i);
+       
+       }
+            System.out.println("Happy new years!");
 
     }
 }
