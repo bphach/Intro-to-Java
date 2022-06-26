f2f266c92d4670d5472d1023b8afb3c7fa15cbb0
bphach <soturota@gmail.com>
2022-06-26 12:25:00 -0400

    commit

--- src/Lesson10_Nested_Loop/main.java
+++ src/Lesson10_Nested_Loop/main.java
@@ -1,8 +1,30 @@
 package Lesson10_Nested_Loop;
 
+import java.util.Scanner;
+
 public class main {
     public static void main(String[] args) {
        
+        // nested loops = a loop inside of a loop
+
+        Scanner scanner = new Scanner(System.in);
+        int rows;
+        int columns;
+        String symbol = "  ";
+
+        System.out.println("Enters # of rows:");
+        rows = scanner.nextInt();
+        System.out.println("Enters # of columns:");
+        columns = scanner.nextInt();
+        System.out.println("Enter symbol to use: ");
+        symbol = scanner.next();
+
+        for(int i=1; i<=rows; i++) {
+            System.out.println();
+            for(int j=1; j<=columns; j++){
+                System.out.print(symbol);
+            }
+        }
 
 
     }
