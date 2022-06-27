c63d077927363c7158ed68d582978dfea299cd63
bphach <soturota@gmail.com>
2022-06-27 12:53:42 -0400

    commit

--- src/Lesson11_Arrays/main.java
+++ src/Lesson11_Arrays/main.java
@@ -3,6 +3,14 @@ package Lesson11_Arrays;
 public class main {
     public static void main(String[] args) {
         
+        // array = used to store multiple values in a single variable
+        // To make an array, next to the data type. Add a set of straight brackets next to it. Surrond the values with a set of curly braces
+        
+        String[] cars = {"Mustang","Corvette","Camaro"};
+
+        cars[0] = "Camaro";
+
+        System.out.println(cars[0]);
 
 
     }
---
+++ src/Lesson14_Wrapper_Class/main.java
@@ -0,0 +1,9 @@
+package Lesson14_Wrapper_Class;
+
+public class main {
+    public static void main(String[] args) {
+       
+
+
+    }
+}
