467d4038b9b1c79ad68da1fb7955197023ee3e9c
bphach <soturota@gmail.com>
2022-06-29 12:36:41 -0400

    commit

--- src/Lesson13_String_Methods/main.java
+++ src/Lesson13_String_Methods/main.java
@@ -2,8 +2,23 @@ package Lesson13_String_Methods;
 
 public class main {
     public static void main(String[] args) {
-       
+       //string = a reference data type that can store one or more characters
+       //         reference data types have access to useful methods 
+
+       String name = "Bryan";
+
+       //boolean result = name.equalsIgnoreCase("Bryan");
+       //int result = name.length();
+       //char result = name.charAt(0);
+       //int result = name.indexOf("y");
+       //boolean result = name.isEmpty();
+       //String result = name.toUpperCase();
+       //String result = name.toLowerCase();
+       //String result = name.trim(); 
+       //String result = name.replace("y","a");
 
 
+
+        System.out.println(result);
     }
 }
